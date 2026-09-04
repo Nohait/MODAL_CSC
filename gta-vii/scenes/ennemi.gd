b1fb5668443b9f7879f30b35932f1f3b92431f68
Eloulord <elouan.vivien@gmail.com>
2026-09-04 17:26:58 +0200

    fix partiel ia ennemi
    tant que je ne sors pas de la zone de detection de ennemi et que j'y rerentre, l'ennemi ne bouge pas, et je suis notifié joueur attaqué

--- gta-vii/scenes/ennemi.gd
+++ gta-vii/scenes/ennemi.gd
@@ -1,8 +1,8 @@
 extends CharacterBody3D
 var cible = null
-var distance_attaque = 1
-var distance_lacher = 6
-var attaque_cooldown = 2
+var distance_attaque = 1.0
+var distance_lacher = 10.0
+var attaque_cooldown = 2.0
 var attaque_timer = 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,28 +11,31 @@ func _ready() -> void:
 func _on_surface_detection_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		cible = body
-		print("Joueur détecté !")
 		
 func _physics_process(delta):
+	
 	attaque_timer -= delta 	#A chaque frame, le cooldown réduit
 	
 	if cible != null:	#Une fois que le joueur est pris pour cible
 		var distance = global_position.distance_to(cible.global_position)
 		
+		
 		if distance > distance_lacher: #calcul de sortie de range
 			cible = null
 			velocity = Vector3.ZERO
+			move_and_slide()	
 			
 		elif distance > distance_attaque: # comportement dans la range
 			var direction = global_position.direction_to(cible.global_position)
 			velocity = direction * 2.0
-			move_and_slide()
+			move_and_slide()	
 			
 		else: #comportement dans la portée d'attaque
 			velocity = Vector3.ZERO 
 			
-			if attaque_timer <0:
+			if attaque_timer < 0.0:
 				attaque()
+			
 			
 func attaque():
 	print("Le joueur est attaqué")
