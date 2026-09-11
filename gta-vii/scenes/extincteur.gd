e89a720e248ae7b17c71f7cc70ec2d35825556ba
Eloulord <elouan.vivien@gmail.com>
2026-09-11 17:09:51 +0200

    Fix (à peu près) de l'attaque
    la taille de l'ennemi est prise en compte dans le cone de detection de l'extincteur
    -> moins de problemes d'attaque

--- gta-vii/scenes/extincteur.gd
+++ gta-vii/scenes/extincteur.gd
@@ -60,16 +60,17 @@ func _physics_process(delta: float) -> void:
 			var origin: Vector3 = muzzle.global_position 
 			var to_target: Vector3 = target_body.global_position - origin
 			var target_direction: Vector3 = to_target.normalized() #direction à l'ennemi
-
+			var body_radius = body.get_node("CollisionShape3D").shape.radius
+			var distance := to_target.length()
 			var forward: Vector3 = (direction_marker.global_position - muzzle.global_position).normalized() #direction de visée du joueur
 
 			var alignment: float = forward.dot(target_direction) #produit scalaire entre les deux directions
-			var minimum_alignment: float = cos(deg_to_rad(cone_angle)) #calcul du cos minimal souhaité
+			var angle = acos(alignment)
 
-			if alignment >= minimum_alignment:
+			if angle <= deg_to_rad(cone_angle) + atan(body_radius / distance):
 				#si l'ennemi est dans le cône, on attaque
 				
-				if Input.is_action_pressed("primary_attack"):
+				if Input.is_action_pressed("primary_attack") and !is_overheated:
 					attaque_1(body)
 
 func attaque_1(cible):
